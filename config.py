50fad50b7dc4acb1c644d8164ed5201f012aba8f
vaproh <vaproh@proton.me>
2024-09-13 11:39:56 +0530

    fixed stuff in config

--- config.py
+++ config.py
@@ -6,19 +6,17 @@ from dotenv import load_dotenv
 import discord
 load_dotenv(dotenv_path="./.env")
 
+# token
+DISCORD_TOKEN: final = os.getenv(
+"DISCORD_TOKEN"
+)
+
+# Lavalink
+lavalink_url: final = os.getenv("LAVALINK_URI")
+lavalink_password: final = os.getenv("LAVALINK_PASSWORD")
+
 # prefix
 prefix: final = "$$"
-
-# logger
-logger = logging.getLogger("bot")
-
-# bot name
-botName: final = os.getenv("BOT_NAME").lower()
-
-# cogs
-cogExt: final = [
-
-]
 
 # Status Cycle
 statuses = [
@@ -33,15 +31,30 @@ activities = [
     discord.Activity(type=discord.ActivityType.listening, name="Vaproh <3"),
 ]
 
+# bot name
+botName: final = os.getenv("BOT_NAME").lower()
 
-# token
-DISCORD_TOKEN: final = os.getenv(
-"DISCORD_TOKEN"
-)
+# colors
+color_main = 0x2D3250 # main color
+color_sec = 0x424769 # secondary color
+color_err = 0x7077A1 # error color
 
-# Lavalink
-lavalink_url: final = os.getenv("LAVALINK_URI")
-lavalink_password: final = os.getenv("LAVALINK_PASSWORD")
+# text
+footer_text = "is {} bot is best right? - 2024".format(botName)
+
+# jishaku envirment variables
+os.environ["JISHAKU_NO_DM_TRACEBACK"] = "True"
+os.environ["JISHAKU_HIDE"] = "True"
+os.environ["JISHAKU_NO_UNDERSCORE"] = "True"
+os.environ["JISHAKU_FORCE_PAGINATOR"] = "True"
+
+# cogs
+cogExt: final = [
+
+]
+
+# logger
+logger = logging.getLogger("bot")
 
 # logger dict and func
 LOGGING_CONFIG = {
@@ -106,20 +119,3 @@ LOGGING_CONFIG = {
 }
 
 dictConfig(LOGGING_CONFIG)
-
-# owner ids
-owner_ids = [575555247557312512, 1195470182831894558, 1092184838225809458]
-
-# colors
-color_main = 0x2D3250 # main color
-color_sec = 0x424769 # secondary color
-color_err = 0x7077A1 # error color
-
-# text
-footer_text = "is {} bot is best right? - 2024".format(botName)
-
-# jishaku envirment variables
-os.environ["JISHAKU_NO_DM_TRACEBACK"] = "True"
-os.environ["JISHAKU_HIDE"] = "True"
-os.environ["JISHAKU_NO_UNDERSCORE"] = "True"
-os.environ["JISHAKU_FORCE_PAGINATOR"] = "True"
